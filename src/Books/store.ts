b5b0ab0f917340f8a45c7a7f5d42c56325e369b2
alx <world.zay@gmail.com>
2025-12-21 14:46:17 +0300

    #4 Move Update sample fx()

--- src/Books/store.ts
+++ src/Books/store.ts
@@ -2,10 +2,18 @@ import { createEffect, createStore, sample } from "effector";
 import ajax from "../common/ajax";
 import type { ApiResponse } from "../common/ajax/types";
 
+type Cover = {
+    bg_img: string | null,
+    cover: string | null,
+    bg_color: string,
+    text_color: string,
+    text_size: number,
+}
+
 type MyBook = {
     id: number,
     title: string,
-    cover: string,
+    cover: Cover | string,
     alias: string,
     genreStr: string,
 }
@@ -19,6 +27,11 @@ export const $myBooks = createStore<MyBook[]>([])
 sample({
     clock: getMyBooksFx.doneData,
     filter: (response) => Boolean(response?.data?.success),
-    fn: (response) => response.data.result,
+    fn: (response) => {
+        return response.data.result.map((book) => {
+            book.cover = JSON.parse(book.cover as string)
+            return book
+        })
+    },
     target: $myBooks,
 })
