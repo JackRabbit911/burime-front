121630250a1f7d3da27913de83ee1466de4f5f0c
alx <world.zay@gmail.com>
2025-12-21 17:01:17 +0300

    #4 Apply Cover components

--- src/Books/components/index.tsx
+++ src/Books/components/index.tsx
@@ -1,18 +1,28 @@
-import { useUnit } from "effector-react"
+import { useList } from "effector-react"
 import { $myBooks, getMyBooksFx } from "../store"
 import { useEffect } from "react"
+import Cover from "./Cover"
+import CoverWrapper from "./CoverWrapper"
 
 const Books = () => {
-  const myBooks = useUnit($myBooks)
-  
+  const myBooks = useList($myBooks, {
+    fn: (book) => (
+      <CoverWrapper role={book.myRole}>
+        <Cover book={book} />
+      </CoverWrapper>
+    ),
+    placeholder: <h2>Здесь пока ничего нет</h2>
+  })
+
   useEffect(() => {
     getMyBooksFx()
   }, [])
-  
-  console.log(myBooks)
+
   return (
     <>
-      My Books list
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 h-auto">
+        {myBooks}
+      </div>
     </>
   )
 }
