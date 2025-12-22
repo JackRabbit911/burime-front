aa33244329c5a0fe595c404ba4f85ef0cc4d4176
alx <world.zay@gmail.com>
2025-12-22 18:18:04 +0300

    #6 Apply Error and Loading components and globalReset

--- src/Branch/components/index.tsx
+++ src/Branch/components/index.tsx
@@ -1,15 +1,32 @@
+import { useEffect } from "react"
 import { useParams } from "react-router"
+import { $bootstrap, getBootstrapFx } from "../store/bootstrap"
+import { useUnit } from "effector-react"
+import Form from "./Form"
+import { $status, globalReset } from "../../common/store"
+import ErrorCmp from "../../reused/ErrorCmp"
+import Loading from "../../reused/Loading"
+import { t } from "../../common/i18n/utils"
 
 const Branch = () => {
   const { id } = useParams()
+  const bootstrap = useUnit($bootstrap)
+  const status = useUnit($status)
 
-  console.log(id)
+  useEffect(() => {
+    globalReset()
+    getBootstrapFx(id)
+  }, [])
 
-  return (
-    <>
-      Create/edit branch {id}
-    </>
-  )
+   if (status >= 400) {
+    return (
+        <ErrorCmp status={status} />
+    )
+  }
+
+  return bootstrap ? (
+    <Form bootstrap={bootstrap}/>
+  ) : <Loading message={t('Loading')} />
 }
 
 export default Branch
