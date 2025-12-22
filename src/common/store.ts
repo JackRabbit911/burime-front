a26ebea00de4cff07834fc939a7a66b431d783be
alx <world.zay@gmail.com>
2025-12-22 15:37:40 +0300

    #6 Add darkMode feature

--- src/common/store.ts
+++ src/common/store.ts
@@ -4,3 +4,19 @@ export const globalReset = createEvent()
 
 export const $status = createStore(200)
     .reset(globalReset)
+
+    export const darkModeChanged = createEvent<boolean>()
+
+export const $darkMode = createStore(initDarkMode())
+    .on(darkModeChanged, (_, darkMode) => darkMode)
+
+function initDarkMode () {
+    const mq = window.matchMedia('(prefers-color-scheme: dark)');
+    mq.addEventListener('change', handleThemeChange);
+
+    return mq.matches
+}
+
+function handleThemeChange(this: MediaQueryList, ev: MediaQueryListEvent) {
+    darkModeChanged(ev.matches)
+}
