51bec2f542b490dd077225dd412bece037b8ebbb
alx <world.zay@gmail.com>
2025-12-22 17:56:20 +0300

    #6 Apply getTitle

--- src/Home/Wrapper.tsx
+++ src/Home/Wrapper.tsx
@@ -1,11 +1,5 @@
 import { Link, useLocation } from "react-router";
-
-const titles: { [key: string]: string } = {
-  home: 'Personal account',
-  books: 'My Books',
-  messages: 'My Messages',
-  authors: 'My Authors',
-}
+import { getTitle } from "./utils";
 
 type Props = {
   children?: React.ReactNode;
@@ -15,12 +9,13 @@ const Wrapper = ({ children }: Props) => {
   const location = useLocation();
   const uriSegments = location.pathname.split('/').filter((v) => Boolean(v))
   const addr = uriSegments[1] ?? 'home'
+  const id = uriSegments[2]
 
   return (
     <div className="flex flex-row justify-center">
       <div className="w-full md:w-2xl lg:w-4xl bg-base-100 p-4">
         <div className="flex flex-roe justify-between mb-4">
-          <h1 className="text-2xl">{titles[addr]}</h1>
+          <h1 className="text-2xl">{getTitle(addr, id)}</h1>
           {addr === 'home' ?
             <a href="/auth/logout" className="btn btn-outline btn-error">Log Out</a> :
             <Link to='/my'>
