edcb86fbb4a016bcdf3d6a5e2e303097f32b1d3a
alx <world.zay@gmail.com>
2025-12-23 16:07:08 +0300

    #5 Update endpoints urls

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,8 +1,8 @@
 export const perPages = [25, 50, 100]
 export const getBootsrapUri = '/my/branch/bootstrap'
 export const getAuthorsUri = '/my/branch/authors'
-export const saveBranchUri = '/my/branch/save'
-export const saveDraftUri = '/my/branch/save/draft'
-export const deleteDraftUri = '/my/branch/delete'
+export const saveBranchUri = '/my/branch/action/savebranch'
+export const saveDraftUri = '/my/branch/action/savedraft'
+export const deleteDraftUri = '/my/branch/action/deldraft'
 
 export const getTranslateUri = '/my/gettranslate'
