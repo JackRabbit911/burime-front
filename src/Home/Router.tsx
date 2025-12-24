6328a4e788baa514f355965a2fec5b34f7858efc
alx <world.zay@gmail.com>
2025-12-24 16:09:13 +0300

    #5 Add route for drafts

--- src/Home/Router.tsx
+++ src/Home/Router.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router"
 import Home from "."
 import Books from "../Books/components"
 import Branch from "../Branch/components"
+import Drafts from "../Drafts/components"
 
 const Router = () => {
   return (
     <Routes>
       <Route path='' element={<Home />} />
       <Route path='books' element={<Books />} />
-      <Route path='drafts' element={<Books />} />
+      <Route path='drafts' element={<Drafts />} />
       <Route path='branch/:id?/:draft?' element={<Branch />} />
     </Routes>
   )
