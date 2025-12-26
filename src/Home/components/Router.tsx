2bac40376f3c7b4c6715885cb627f31e74f63069
alx <world.zay@gmail.com>
2025-12-26 15:39:08 +0300

    #12 Add author abd authors routes

--- src/Home/components/Router.tsx
+++ src/Home/components/Router.tsx
@@ -3,14 +3,18 @@ import Home from "."
 import Books from "../../Books/components"
 import Branch from "../../Branch/components"
 import Drafts from "../../Drafts/components"
+import Authors from "../../Authors/components"
+import AuthorFormWrapper from "../../Authors/components/AuthorFormWrapper"
 
 const Router = () => {
   return (
     <Routes>
       <Route path='' element={<Home />} />
       <Route path='books' element={<Books />} />
+      <Route path='authors' element={<Authors />} />
       <Route path='drafts' element={<Drafts />} />
       <Route path='branch/:id?/:draft?' element={<Branch />} />
+      <Route path='author/:id?' element={<AuthorFormWrapper />} />
     </Routes>
   )
 }
