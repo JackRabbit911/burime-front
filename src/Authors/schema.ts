1825b07d82f93d09d24485afd472d0349c0248a0
alx <world.zay@gmail.com>
2025-12-27 10:25:18 +0300

    #12 Update Authors/schema

--- src/Authors/schema.ts
+++ src/Authors/schema.ts
@@ -1,13 +1,15 @@
 import * as z from "zod"
+import { imageFile } from "../common/schema.ts/files"
 
 export const authorSchema = z.object({
     id: z.number(),
     alias: z.string().trim().min(1, { message: 'Required' }).regex(/^[^<>;]*$/, 'Invalid input!'),
     slogan: z.string().regex(/^[^<>;]*$/, 'Invalid input!'),
     info: z.string().regex(/^[^<>;]*$/, 'Invalid input!'),
-    openclosed: z.number(),
+    openclosed: z.coerce.number(),
     owner: z.boolean(),
     avatar: z.string(),
+    file: imageFile.nullish(),
 })
 
 export type MyAuthor = z.infer<typeof authorSchema>
