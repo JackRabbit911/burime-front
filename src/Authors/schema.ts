8720095783707258abc3ee93f4e1512065785218
alx <world.zay@gmail.com>
2025-12-28 11:51:14 +0300

    #12 Add authorOut schema

--- src/Authors/schema.ts
+++ src/Authors/schema.ts
@@ -2,14 +2,22 @@ import * as z from "zod"
 import { imageFile } from "../common/schema.ts/files"
 
 export const authorSchema = z.object({
-    id: z.number(),
+    id: z.number().optional(),
     alias: z.string().trim().min(1, { message: 'Required' }).regex(/^[^<>;]*$/, 'Invalid input!'),
-    slogan: z.string().regex(/^[^<>;]*$/, 'Invalid input!'),
-    info: z.string().regex(/^[^<>;]*$/, 'Invalid input!'),
+    info: z.object({
+        slogan: z.string().regex(/^[^<>;]*$/, 'Invalid input!'),
+        info: z.string().regex(/^[^<>;]*$/, 'Invalid input!'),
+    }),
     openclosed: z.coerce.number(),
-    owner: z.boolean(),
-    avatar: z.string(),
+    owner: z.boolean().optional(),
+    avatar: z.string().optional(),
     file: imageFile.nullish(),
 })
 
+export const authorOut = authorSchema.omit({
+    owner: true,
+    avatar: true,
+})
+
 export type MyAuthor = z.infer<typeof authorSchema>
+export type MyAuthorOut = z.infer<typeof authorOut>
