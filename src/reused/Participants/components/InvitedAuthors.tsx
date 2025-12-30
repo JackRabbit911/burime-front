77d13f566b2dfb7b354ed94528ceafe343ae2b62
alx <world.zay@gmail.com>
2025-12-30 17:46:48 +0300

    #12 Create InvitedAuthors component

---
+++ src/reused/Participants/components/InvitedAuthors.tsx
@@ -0,0 +1,37 @@
+import { useFormContext } from "react-hook-form";
+import type { Member } from "../schema";
+import { memberIdSetted } from "../store/authors";
+
+type Props = {
+  author: Member;
+}
+
+const InvitedAuthors = ({ author }: Props) => {
+  const { setValue, watch } = useFormContext()
+
+  const deleteMember = (author: Member) => () => {
+    const members = watch('members').filter((item: Member) => item.id !== author.id)
+    setValue('members', members)
+  }
+
+  return (
+    <div className="flex flex-row justify-between gap-2">
+      <button
+        type="button"
+        className="btn btn-soft btn-outline btn-xs grow"
+        onClick={() => memberIdSetted(author.id)}
+      >
+        {author.alias}
+      </button>
+      <button
+        type="button"
+        className="btn btn-soft btn-square btn-xs"
+        onClick={deleteMember(author)}
+      >
+        <span className=" text-red-600">X</span>
+      </button>
+    </div>
+  )
+}
+
+export default InvitedAuthors
