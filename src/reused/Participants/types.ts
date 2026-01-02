dea2596f45194cff48942ad4147772d696b091d2
alx <world.zay@gmail.com>
2026-01-02 13:11:26 +0300

    #12 Update reused types

--- src/reused/Participants/types.ts
+++ src/reused/Participants/types.ts
@@ -1,11 +1,13 @@
-export type OwnAuthor = {
-    id: number;
-    alias: string;
-}
-
 export type Member = {
     id: number;
     role: number;
     status: number;
     alias: string;
+}
+
+export type AuthorsPayload = {
+    page: number;
+    limit: number;
+    filter: string | null;
+    search: string | null;
 }
