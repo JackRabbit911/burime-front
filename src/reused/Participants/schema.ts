694e9eca1f7fcd38fc2750c5c6d4ff0b13777e0c
alx <world.zay@gmail.com>
2026-01-02 13:12:15 +0300

    #12 Update reused/Participants schema

--- src/reused/Participants/schema.ts
+++ src/reused/Participants/schema.ts
@@ -1,23 +1,6 @@
 import * as z from "zod"
 
-const authorsFilters = z.array(z.string())
-const authorsPermissions = z.object({}).catchall(z.number())
-const authorsStatuses = z.object({}).catchall(z.number())
-
-export const member = z.object({
-    id: z.number().int().positive(),
-    role: z.number().int().min(0).max(255),
-    status: z.number().int().min(0).max(255),
-    alias: z.string(),
-})
-
-export const slimMember = member.transform((input) => ({
-    author_id: input.id,
-    role: input.role,
-    status: input.status,
-}))
-
-const authorsFilterSearch = z.string()
+export const authorsFilterSearch = z.string()
     .trim()
     .regex(/^[^<>]*$/, 'Invalid input!')
     .nullable()
@@ -33,21 +16,6 @@ export const authorsSch = z.object({
     count: z.number().nonnegative().int(),
 })
 
-export const authorsPayload = z.object({
-    filter: z.optional(authorsFilterSearch),
-    search: z.optional(authorsFilterSearch),
-    page: z.optional(z.number().positive()),
-    limit: z.optional(z.number().positive()),
-}).optional()
 
-export const ownAuthors = z.array(author)
-
-export type Member = z.infer<typeof member>
 export type Author = z.infer<typeof author>
 export type Authors = z.infer<typeof authorsSch>
-export type OwnAuthors = z.infer<typeof ownAuthors>
-export type AuthorsPayload = z.infer<typeof authorsPayload>
-
-export type AuthorsFilters = z.infer<typeof authorsFilters>
-export type Statuses = z.infer<typeof authorsStatuses>
-export type Permissions = z.infer<typeof authorsPermissions>
