e5dd70eda3a2430dee7cf9a199db4508e7a78baa
alx <world.zay@gmail.com>
2026-01-04 16:07:01 +0300

    #14 Update imports and constants, apply AuthorsChoice reused component

--- src/Branch/components/Authors/components/AuthorsWrapper/index.tsx
+++ src/Branch/components/Authors/components/AuthorsWrapper/index.tsx
@@ -1,13 +1,32 @@
+import { useFormContext } from "react-hook-form";
+import { useUnit } from "effector-react";
 import Members from "./components/Members";
-import AuthorsChoice from "./components/AuthorsChoice";
 import Select from "./components/Select";
 import type { Bootstrap } from "../../../../schema/input";
+import type { Author } from "../../../../../reused/Participants/schema";
+import { addNewMember } from "../../../../../reused/Participants/utils";
+import { $authors1 } from "../../../../../reused/Participants/store/authors";
+import { $referenceBooks } from "../../../../../reused/Participants/store/reference";
+import AuthorsChoice from "../../../../../reused/Participants/components/AuthorsChoice";
+import { $authorsPayload } from "../../../../../reused/Participants/store/athorsPayload";
 
 type Props = {
   bootstrap: Bootstrap;
 }
 
 const AuthorsWrapper = ({ bootstrap }: Props) => {
+  const authors = useUnit($authors1)
+  const authorsPayload = useUnit($authorsPayload)
+  const referenceBooks = useUnit($referenceBooks)
+  const authorsFilters = referenceBooks?.authorsFilters
+
+  const { getValues, setValue } = useFormContext()
+  const members = getValues('members') || []
+
+  const onChoice = (author: Author) => {
+    const newMembers = addNewMember(members, author)
+    setValue('members', newMembers, { shouldValidate: true, shouldDirty: true })
+  }
 
   return (
     <>
@@ -23,7 +42,11 @@ const AuthorsWrapper = ({ bootstrap }: Props) => {
       </fieldset>
       <div className="md:col-span-2">
         <AuthorsChoice
-          filters={bootstrap.authorsFilters}
+          filters={authorsFilters as string[]}
+          authors={authors?.list}
+          members={members}
+          authorsPayload={authorsPayload}
+          handler={onChoice}
         />
       </div>
     </>
