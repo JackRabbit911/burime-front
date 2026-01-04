d4fcb58d78ae5b77c336fad8c966bceb7878351f
alx <world.zay@gmail.com>
2026-01-04 12:09:37 +0300

    #12 Add handleCheck function and apply Status component

--- src/MyAuthors/components/Participants/MembersPermissions.tsx
+++ src/MyAuthors/components/Participants/MembersPermissions.tsx
@@ -1,10 +1,13 @@
-import { useFormContext } from "react-hook-form";
 import { useUnit } from "effector-react";
+import { useFormContext } from "react-hook-form";
+import { t } from "../../../common/i18n/utils";
+import type { Member } from "../../../reused/Participants/types";
 import { getCurrentMember } from "../../../reused/Participants/utils";
+import { memberIdResetted } from "../../../reused/Participants/store/authors";
+import { $referenceBooks } from "../../../reused/Participants/store/reference";
 import PermissionsList from "../../../reused/Participants/components/Permissions/PermissionsList";
 import Participants from "../../../reused/Participants/components/Permissions/Participants";
-import { memberIdResetted } from "../../../reused/Participants/store/authors";
-import { t } from "../../../common/i18n/utils";
+import Status from "./Status";
 
 type Props = {
   authorId: number;
@@ -12,9 +15,24 @@ type Props = {
 
 const MembersPermissions = ({ authorId }: Props) => {
   const onClose = useUnit(memberIdResetted)
-  const { getValues } = useFormContext()
+  const referenceBooks = useUnit($referenceBooks)
+  const authorsPermissions = referenceBooks?.authorsPermissions
+
+  const { getValues, setValue } = useFormContext()
   const members = getValues('members') || []
   const currentAuthor = getCurrentMember(members, authorId)
+
+  const handleCheck = (val: number, id: number, isAdd: boolean) => {
+      const newMembers = members.map((value: Member) => {
+        if (value.id === id) {
+          value.role = isAdd ? value.role | val : value.role &= ~val
+        }
+  
+        return value
+      })
+  
+      setValue('members', newMembers)
+    }
 
   return (
     <>
@@ -33,12 +51,15 @@ const MembersPermissions = ({ authorId }: Props) => {
         <fieldset className="fieldset">
           <PermissionsList
             member={currentAuthor}
+            handler={handleCheck}
+            permissions={authorsPermissions}
           />
         </fieldset>
         <fieldset className="fieldset">
-          {/* <Status
+          <Status
             member={currentAuthor}
-          /> */}
+            statuses={referenceBooks?.authorsStatuses}
+          />
         </fieldset>
         <button className="md:col-span-2 btn btn-sm"
           onClick={onClose}
