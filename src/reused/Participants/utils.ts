5ce93736f1fd4a66540db04cc2e3c02e2a61ebee
alx <world.zay@gmail.com>
2026-01-08 16:29:05 +0300

    #14 Add chancgeMaster function

--- src/reused/Participants/utils.ts
+++ src/reused/Participants/utils.ts
@@ -1,5 +1,5 @@
 import type { Author } from "./schema"
-import type { Member} from "./types"
+import type { Member } from "./types"
 
 export const getCurrentMember = (members: Member[], id: number): Member | null => {
     if (members.length === 0) {
@@ -41,3 +41,36 @@ export const addNewMember = (
 }
 
 export const isPermission = (role: number, permission: number) => (role & permission) !== 0 ? true : false
+
+export const changeMaster = (members: Member[], ownAuthors: Author[], masterId: number) => {
+    const ownAuthorsIds: number[] = []
+    let masterAlias = ''
+
+    ownAuthors.forEach((ownAuthor) => {
+        ownAuthorsIds.push(ownAuthor?.id)
+
+        if (ownAuthor.id === Number(masterId)) {
+            masterAlias = ownAuthor.alias
+        }
+    })
+
+    const result = members.map((member) => {
+        if (ownAuthorsIds.includes(member.id)) {
+            member.id = Number(masterId)
+            member.alias = masterAlias
+        }
+
+        return member
+    })
+
+    if (members.length === 0) {
+        result.push({
+            id: masterId,
+            role: 255,
+            status: 200,
+            alias: masterAlias,
+        })
+    }
+
+    return result;
+}
