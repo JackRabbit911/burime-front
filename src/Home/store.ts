c51999db415aaf8a15bd08dd51ed419a1be16dc0
alx <world.zay@gmail.com>
2026-01-10 15:00:00 +0300

    #14 Move MyStat type to common module

--- src/Home/store.ts
+++ src/Home/store.ts
@@ -2,23 +2,7 @@ import { createEffect, createStore, sample } from "effector";
 import ajax from "../common/ajax";
 import type { ApiResponse } from "../common/ajax/types";
 import { getMyStatUri } from "../common/constants";
-
-type MyStat = {
-    books: {
-        total: number;
-        own: number;
-    };
-    drafts: number;
-    authors: {
-        total: number;
-        own: number;
-    };
-    messages: {
-        total: number;
-        new: number;
-    };
-    complete: number;
-}
+import type { MyStat } from "../common/types";
 
 export const getMyStatFx = createEffect(() => (
     ajax.get<ApiResponse<MyStat>>(getMyStatUri)
