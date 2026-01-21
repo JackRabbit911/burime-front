c0e9f4ddf508b43b9b3f90eed794bb6fc03e8f38
alx <world.zay@gmail.com>
2026-01-21 10:13:40 +0300

    #16 Create deleteDialog function

---
+++ src/common/utils/deleteDialog.ts
@@ -0,0 +1,18 @@
+import { confirmDialog } from "../../reused/InModal/ConfirmDialog"
+import { closeBtn, modalOpened } from "../../reused/Modal/store"
+import ajax from "../ajax"
+
+const yes = async (uri: string) => {
+    const response = await ajax.delete(uri)
+    const { result } = response.data
+    closeBtn(true)
+    modalOpened(result)
+}
+
+export const deleteDialog = (uri: string, text: string, link?: string) => {
+    modalOpened(confirmDialog({
+        text: text,
+        onYes: () => yes(uri),
+        link: link,
+    }))
+}
