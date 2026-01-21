3b53616e2f4861de28de9678d25e045e4394d0ac
alx <world.zay@gmail.com>
2026-01-21 10:19:13 +0300

    #16 Add deleteMsg and removeMsg functions

--- src/Message/utils.ts
+++ src/Message/utils.ts
@@ -1,12 +1,29 @@
+import { deleteMessageUri, removeMessageUri } from "../common/constants"
+import { deleteDialog } from "../common/utils/deleteDialog"
 import { defaultMsg } from "./components/Templates/DefaultMsg"
 import type { Message } from "./types"
 
 export const getComponent = (message: Message) => {
     const key = message.data?.tpl ?? ''
-    message.data.body = message.data.body.replace('{AUTHOR}', message.to_alias)
     
     switch (key) {
         default:
-            return defaultMsg({ data: message.data })
+            return defaultMsg({ message })
     }
+}
+
+export const removeMsg = (id: string | undefined) => {
+    const uri = [removeMessageUri, id].join('/')
+    const text = 'Your sended message will be deleted from everyone!'
+    const link = '/message/list'
+
+    deleteDialog(uri, text, link)
+}
+
+export const deleteMsg = (id: string | undefined) => {
+    const uri = [deleteMessageUri, id].join('/')
+    const text = 'This message will be deleted'
+    const link = '/message/list'
+
+    deleteDialog(uri, text, link)
 }
