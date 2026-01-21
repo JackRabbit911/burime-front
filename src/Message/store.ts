d6b973fcfc8dac63727be6cb1f1d357bcfd7e413
alx <world.zay@gmail.com>
2026-01-21 10:16:35 +0300

    #16 Add $tiAlias store and hamdling

--- src/Message/store.ts
+++ src/Message/store.ts
@@ -1,8 +1,11 @@
-import { createEffect, createStore } from "effector";
+import { createEffect, createEvent, createStore, sample } from "effector";
 import ajax from "../common/ajax";
 import type { ApiResponse } from "../common/ajax/types";
 import type { Message, Inbox, MessageList, Outbox } from "./types";
 import { getMessageListUri, getMessageUri } from "../common/constants";
+
+export const msgResetted = createEvent()
+export const toAliasSetted = createEvent<string>('')
 
 export const getMessageListFx = createEffect(
     () => ajax.get<ApiResponse<MessageList>>(getMessageListUri)
@@ -20,3 +23,20 @@ export const $outbox = createStore<Outbox[]>([])
 
 export const $message = createStore<Message | null>(null)
     .on(getMessageFx.doneData, (_, response) => response.data.result)
+    .reset(msgResetted)
+
+export const $toAlias = createStore<string>('Author')
+
+sample({
+    clock: toAliasSetted,
+    source: $message,
+    filter: (message) => typeof (message?.to) === 'object',
+    fn: (message, alias) => {
+        if (typeof (message?.to) === 'object') {
+            return alias
+        }
+
+        return 'Author'
+    },
+    target: $toAlias
+})
