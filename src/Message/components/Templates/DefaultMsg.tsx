383c5c374595ee2713effa4aec42afe631d3f52e
alx <world.zay@gmail.com>
2026-01-21 10:26:30 +0300

    #16 Update template

--- src/Message/components/Templates/DefaultMsg.tsx
+++ src/Message/components/Templates/DefaultMsg.tsx
@@ -1,12 +1,30 @@
-import type { MsgData } from "../../types"
+import { useUnit } from "effector-react";
+import type { Message } from "../../types"
+import { $toAlias } from "../../store";
+import { t } from "../../../common/i18n/utils";
 
 type Props = {
-    data: MsgData;
+  message: Message;
 }
 
-const DefaultMsg = ({ data }: Props) => {
-    return <>{data?.body ?? 'no body'}</>
+const DefaultMsg = ({ message }: Props) => {
+  const toAlias = useUnit($toAlias);
+  const appeal = message.incoming && Object.hasOwn(message, 'to_alias') ? message.to_alias : toAlias
+
+  return (
+    <>
+      <p>
+        {t('Dear')}, {appeal},
+      </p>
+      <p>
+        {message.data?.body ?? 'no body'}
+      </p>
+      <div className="fieldset mt-1 text-end">
+        {t('Best regards')}, {message.from_alias}
+      </div>
+    </>
+  )
 }
 
-export const defaultMsg = (props: Props) => <DefaultMsg { ...props } />
+export const defaultMsg = (props: Props) => <DefaultMsg {...props} />
 export default DefaultMsg
