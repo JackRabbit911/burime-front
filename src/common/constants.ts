24298a61e47740e9770ee66dbfab40622fc3c1c2
alx <world.zay@gmail.com>
2026-01-23 12:45:17 +0300

    #16 Add getMsgReferenceUri endpoint as constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -24,4 +24,5 @@ export const getMessageListUri = 'my/message/list'
 export const getMessageUri = 'my/message/show'
 export const removeMessageUri = 'my/message/remove'
 export const deleteMessageUri = 'my/message/delete'
+export const getMsgReferenceUri = '/my/reference/message'
 
