4f5d7d2c4272fb8191e8e6bf803c25aca4c73504
alx <world.zay@gmail.com>
2026-01-28 11:46:14 +0300

    #16 Create conditional components

---
+++ src/Message/components/Templates/CheckboxConditional.tsx
@@ -0,0 +1,24 @@
+import type { ChangeEvent } from "react";
+// import { t } from "../../../common/i18n/utils"
+
+type Props = {
+  label: string;
+  isChecked: boolean;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const CheckboxConditional = ({ label, isChecked, onChange }: Props) => {
+  return (
+    <label className="fieldset-label flex">
+      <input
+        type="checkbox"
+        className="checkbox checkbox-sm"
+        checked={isChecked}
+        onChange={onChange}
+      />
+      {label}
+    </label>
+  )
+}
+
+export default CheckboxConditional
