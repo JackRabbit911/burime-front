cd806c2bdf4ec08f7e387acac5fae229848d4eec
alx <world.zay@gmail.com>
2026-01-28 15:42:19 +0300

    #16 Add form tag as wrapper and apply useMessageForm custom hook

--- src/Message/components/Form/MessageFormWrapper.tsx
+++ src/Message/components/Form/MessageFormWrapper.tsx
@@ -1,72 +1,54 @@
 import Grid3Cols from "../../../reused/Wrapper/Grid3Cols"
-import { FormProvider, useForm } from "react-hook-form"
+import { FormProvider } from "react-hook-form"
 import AuthorsChoiceWrapper from "./AuthorsChoiceWrapper"
 import Recipients from "./Recipients"
-import { useUnit } from "effector-react"
-import { $ownAuthors, getOwnAuthorsFx } from "../../../common/store/ownAuthors"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Select from "../../../reused/Participants/components/Select"
 import Controls from "./Controls"
 import Form from "."
-import { zodResolver } from "@hookform/resolvers/zod"
-import { messageForm } from "../../schema"
 import CheckBox from "../../../reused/CheckBox"
 import { t } from "../../../common/i18n/utils"
+import { useMessageForm } from "../../hooks"
 
 const MessageFormWrapper = () => {
-  const ownAuthors = useUnit($ownAuthors)
   const [view, setView] = useState('choice')
-
-  const methods = useForm({
-    resolver: zodResolver(messageForm),
-    mode: 'all',
-    defaultValues: {
-      recipients: [],
-      message: {},
-      important: false,
-    }
-  })
-
   const handleSwitchBtn = (data: string) => {
     setView(data)
   }
 
-  useEffect(() => {
-    getOwnAuthorsFx()
-  }, [])
-
-  useEffect(() => {
-    if (ownAuthors.length > 0) {
-      methods.setValue('message.from', ownAuthors[0].id || 0)
-    }
-
-  }, [ownAuthors])
+  const { methods, ownAuthors, onSubmit } = useMessageForm()
 
   return (
     <FormProvider {...methods}>
-      <Grid3Cols>
-        <div>
-          <Select
-            fieldName="message.from"
-            label="Message sender"
-            options={ownAuthors}
-          />
-          <div className="mt-2">
-            <CheckBox
-              fieldName="important"
-              label={t('Mark as important')}
+      <form
+        onSubmit={methods.handleSubmit(onSubmit)}
+      >
+        <Grid3Cols>
+          <div>
+            <Select
+              fieldName="message.from"
+              label="Message sender"
+              options={ownAuthors}
             />
+            <div className="mt-2">
+              <CheckBox
+                fieldName="important"
+                label={t('Mark as important')}
+              />
+            </div>
+            <Recipients />
           </div>
-          <Recipients />
-        </div>
-        {view === 'form' ? <Form /> :
-          <AuthorsChoiceWrapper />
-        }
-      </Grid3Cols>
-      <Controls
-        view={view}
-        setView={handleSwitchBtn}
-      />
+          <div className="md:col-span-2">
+            {view === 'form' ? <Form /> :
+              <AuthorsChoiceWrapper />
+            }
+          </div>
+        </Grid3Cols>
+        <Controls
+          view={view}
+          setView={handleSwitchBtn}
+        />
+      </form>
     </FormProvider>
   )
 }
