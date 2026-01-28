97d1d5804ced4c324735ee02efa439f451be2d91
alx <world.zay@gmail.com>
2026-01-28 15:40:09 +0300

    #16 Add useMessageForm hook

--- src/Message/hooks.ts
+++ src/Message/hooks.ts
@@ -1,10 +1,15 @@
-import { useFormContext } from "react-hook-form"
-import type { NewRecipient } from "./schema"
+import { useForm, useFormContext, type SubmitHandler } from "react-hook-form"
+import { messageForm, messageOut, type MessageForm, type NewRecipient } from "./schema"
 import { helpBtnClicked } from "../reused/Help/store"
 import { modalOpened } from "../reused/Modal/store"
 import { isObjectEmpty } from "../common/utils"
 import { helper } from "../reused/Help"
 import { confirmDialog } from "../reused/InModal/ConfirmDialog"
+import { useUnit } from "effector-react"
+import { $ownAuthors, getOwnAuthorsFx } from "../common/store/ownAuthors"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { msgSubmitted } from "./store"
+import { useEffect } from "react"
 
 export const useControls = (setView: (data: string) => void) => {
     const { getValues, watch, formState: { errors } } = useFormContext()
@@ -49,4 +54,44 @@ export const useControls = (setView: (data: string) => void) => {
         submitDisabled,
         formBtnDisabled
     }
+}
+
+export const useMessageForm = () => {
+    const ownAuthors = useUnit($ownAuthors)
+    
+      const methods = useForm({
+        resolver: zodResolver(messageForm),
+        mode: 'all',
+        defaultValues: {
+          recipients: [],
+          message: {},
+          important: false,
+        }
+      })
+    
+    
+      const onSubmit: SubmitHandler<MessageForm> = (data) => {
+        const valid = messageOut.safeParse(data)
+    
+        if (valid?.error) {
+          console.log(valid.error, data)
+        }
+    
+        if (valid?.success && valid?.data) {
+          msgSubmitted(valid.data)
+        }
+      }
+    
+      useEffect(() => {
+        getOwnAuthorsFx()
+      }, [])
+    
+      useEffect(() => {
+        if (ownAuthors.length > 0) {
+          methods.setValue('message.from', ownAuthors[0].id || 0)
+        }
+    
+      }, [ownAuthors])
+
+      return { methods, ownAuthors, onSubmit }
 }
