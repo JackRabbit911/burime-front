c3b66c867189f27920b7970c6dc881a882669ded
alx <world.zay@gmail.com>
2026-01-28 11:40:30 +0300

    #16 Create useControls custom hook

---
+++ src/Message/hooks.ts
@@ -0,0 +1,52 @@
+import { useFormContext } from "react-hook-form"
+import type { NewRecipient } from "./schema"
+import { helpBtnClicked } from "../reused/Help/store"
+import { modalOpened } from "../reused/Modal/store"
+import { isObjectEmpty } from "../common/utils"
+import { helper } from "../reused/Help"
+import { confirmDialog } from "../reused/InModal/ConfirmDialog"
+
+export const useControls = (setView: (data: string) => void) => {
+    const { getValues, watch, formState: { errors } } = useFormContext()
+    const recipients = getValues('recipients') as NewRecipient[]
+
+    const onHelpClick = (path: string) => () => {
+        helpBtnClicked(path)
+        modalOpened(helper({ path }))
+    }
+
+    const onChoiceClick = () => {
+        setView('choice')
+    }
+
+    const onFormClick = () => {
+        setView('form')
+    }
+
+    const onCancel = () => {
+        modalOpened(
+            confirmDialog({
+                text: 'Message creation will be cancelled',
+                link: '/message/list',
+            })
+        )
+    }
+
+    const submitDisabled = (
+        !isObjectEmpty(errors) ||
+        recipients.length === 0 ||
+        watch('message.subject') === '' ||
+        watch('message.subject') === undefined
+    )
+
+    const formBtnDisabled = recipients.length === 0
+
+    return {
+        onHelpClick,
+        onChoiceClick,
+        onFormClick,
+        onCancel,
+        submitDisabled,
+        formBtnDisabled
+    }
+}
