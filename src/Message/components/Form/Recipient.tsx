f08f8453de0962632aaf57076f17a7cb43787b0d
alx <world.zay@gmail.com>
2026-01-28 12:18:05 +0300

    #16 Add onClick feature

--- src/Message/components/Form/Recipient.tsx
+++ src/Message/components/Form/Recipient.tsx
@@ -2,17 +2,22 @@ import type { Author } from "../../../reused/Participants/schema";
 
 type Props = {
   author: Author;
+  onClick: (alias: string) => void;
   onDelete: (member: Author) => void;
 }
 
-const Recipient = ({ author, onDelete }: Props) => {
+const Recipient = ({ author, onClick, onDelete }: Props) => {
+
   return (
     <div className="flex flex-row justify-between gap-2">
-      <span
+      <button
         className="btn btn-soft btn-outline btn-xs grow"
+        onClick={() => {
+          onClick(author.alias)
+        }}
       >
         {author.alias}
-      </span>
+      </button>
       <button
         type="button"
         className="btn btn-soft btn-square btn-xs"
