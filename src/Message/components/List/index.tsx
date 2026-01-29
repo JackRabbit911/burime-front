370b8774da33c5355563f5ed96f0877e1c1fef82
alx <world.zay@gmail.com>
2026-01-29 17:29:06 +0300

    #16 Apply Inbox, Outbox, Delbox components

--- src/Message/components/List/index.tsx
+++ src/Message/components/List/index.tsx
@@ -1,41 +1,29 @@
-import { useEffect, useState } from "react"
-import { $delbox, $inbox, $msgCounts, $outbox, getMessageListFx, msgResetted } from "../../store"
-import { useList, useUnit } from "effector-react"
-import { t } from "../../../common/i18n/utils"
-import InMsgLine from "./InMsgLine"
-import OutMsgLine from "./OutMsgLine"
-import TableHead from "./TableHead"
+import { useEffect } from "react"
+import { getMessageListFx, msgResetted } from "../../store"
 import { Link } from "react-router"
-import DelMsgLine from "./DelMsgLine"
+import Inbox from "./Inbox"
+import Outbox from "./Outbox"
+import Delbox from "./Delbox"
 
-const List = () => {
-  const inbox = useList($inbox, (message) => <InMsgLine message={message} />)
-  const outbox = useList($outbox, (message) => <OutMsgLine message={message} />)
-  const delbox = useList($delbox, (message) => <DelMsgLine message={message} />)
+type Props = {
+  box: string;
+}
 
-  const { inboxCount, outboxCount, delboxCount } = useUnit($msgCounts)
-  const [tab, setTab] = useState('inbox')
+const List = ({ box }: Props) => {
+  let component
 
-  let inboxClass = "tab"
-  let outboxClass = "tab"
-  let delboxClass = "tab"
-
-  switch (tab) {
-    case ('inbox'):
-      inboxClass = "tab tab-active"
-      outboxClass = "tab"
-      delboxClass = "tab"
+  switch (box) {
+    case 'inbox':
+      component = <Inbox />
       break
-    case ('outbox'):
-      inboxClass = "tab"
-      outboxClass = "tab tab-active"
-      delboxClass = "tab"
+    case 'outbox':
+      component = <Outbox />
       break
-    case ('deleted'):
-      inboxClass = "tab"
-      outboxClass = "tab"
-      delboxClass = "tab tab-active"
+    case 'deleted':
+      component = <Delbox />
       break
+    default:
+      component = `Invalid segment: ${box}`
   }
 
   useEffect(() => {
@@ -43,6 +31,7 @@ const List = () => {
     getMessageListFx()
   }, [])
 
+  
   return (
     <>
       <div className="text-end">
@@ -52,57 +41,7 @@ const List = () => {
           </button>
         </Link>
       </div>
-      <div role="tablist" className="tabs tabs-lift w-full">
-        <a
-          role="tab"
-          className={inboxClass}
-          onClick={() => setTab('inbox')}
-        >
-          {t('Inbox')} ({inboxCount})
-        </a>
-        <div role="tabpanel" className="w-full tab-content border-base-300 rounded-btn p-6">
-          <table className="table">
-            <TableHead />
-            <tbody>
-              {inbox}
-            </tbody>
-          </table>
-        </div>
-        <a
-          role="tab"
-          className={outboxClass}
-          onClick={() => setTab('outbox')}
-        >
-          {t('Outbox')} ({outboxCount})
-        </a>
-        <div role="tabpanel" className="w-full tab-content border-base-300 rounded-btn p-6">
-          <table className="table">
-            <TableHead />
-            <tbody>
-              {outbox}
-            </tbody>
-          </table>
-        </div>
-        <a
-          role="tab"
-          className={delboxClass}
-          onClick={() => setTab('deleted')}
-        >
-          {t('Deleted')} ({delboxCount})
-        </a>
-        <div role="tabpanel" className="w-full tab-content border-base-300 rounded-btn p-6">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>{t('Subject')}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {delbox}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      {component}
     </>
   )
 }
