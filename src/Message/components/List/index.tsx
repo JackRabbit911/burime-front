8ffb6545b09b9a80788eabbe728bc6d4b891fe1f
alx <world.zay@gmail.com>
2026-01-29 15:02:54 +0300

    #16 Add deleted tab

--- src/Message/components/List/index.tsx
+++ src/Message/components/List/index.tsx
@@ -1,30 +1,42 @@
 import { useEffect, useState } from "react"
-import { $inbox, $msgCounts, $outbox, getMessageListFx, msgResetted } from "../../store"
+import { $delbox, $inbox, $msgCounts, $outbox, getMessageListFx, msgResetted } from "../../store"
 import { useList, useUnit } from "effector-react"
 import { t } from "../../../common/i18n/utils"
 import InMsgLine from "./InMsgLine"
 import OutMsgLine from "./OutMsgLine"
 import TableHead from "./TableHead"
 import { Link } from "react-router"
+import DelMsgLine from "./DelMsgLine"
 
 const List = () => {
   const inbox = useList($inbox, (message) => <InMsgLine message={message} />)
   const outbox = useList($outbox, (message) => <OutMsgLine message={message} />)
-  const { inboxCount, outboxCount } = useUnit($msgCounts)
+  const delbox = useList($delbox, (message) => <DelMsgLine message={message} />)
+
+  const { inboxCount, outboxCount, delboxCount } = useUnit($msgCounts)
   const [tab, setTab] = useState('inbox')
+
   let inboxClass = "tab"
-  let ouboxClass = "tab"
+  let outboxClass = "tab"
+  let delboxClass = "tab"
 
   switch (tab) {
     case ('inbox'):
       inboxClass = "tab tab-active"
-      ouboxClass = "tab"
+      outboxClass = "tab"
+      delboxClass = "tab"
       break
     case ('outbox'):
       inboxClass = "tab"
-      ouboxClass = "tab tab-active"
+      outboxClass = "tab tab-active"
+      delboxClass = "tab"
+      break
+    case ('deleted'):
+      inboxClass = "tab"
+      outboxClass = "tab"
+      delboxClass = "tab tab-active"
+      break
   }
-
 
   useEffect(() => {
     msgResetted()
@@ -58,7 +70,7 @@ const List = () => {
         </div>
         <a
           role="tab"
-          className={ouboxClass}
+          className={outboxClass}
           onClick={() => setTab('outbox')}
         >
           {t('Outbox')} ({outboxCount})
@@ -68,6 +80,25 @@ const List = () => {
             <TableHead />
             <tbody>
               {outbox}
+            </tbody>
+          </table>
+        </div>
+        <a
+          role="tab"
+          className={delboxClass}
+          onClick={() => setTab('deleted')}
+        >
+          {t('Deleted')} ({delboxCount})
+        </a>
+        <div role="tabpanel" className="w-full tab-content border-base-300 rounded-btn p-6">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>{t('Subject')}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {delbox}
             </tbody>
           </table>
         </div>
