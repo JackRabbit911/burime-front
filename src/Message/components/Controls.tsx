059a9c228fa1e1dbc8b9b40581f2b1dfb2426fd6
alx <world.zay@gmail.com>
2026-01-29 14:56:19 +0300

    #16 Create reused component Controls

---
+++ src/Message/components/Controls.tsx
@@ -0,0 +1,31 @@
+import { Link, useParams } from "react-router"
+import { removeMsg } from "../utils"
+import { t } from "../../common/i18n/utils"
+
+type Props = {
+  label: string;
+}
+
+const Controls = ({ label }:Props) => {
+  const { id } = useParams()
+
+  return (
+    <div className="flex justify-end gap-2 mt-1">
+      <Link to="/message/list">
+        <button
+          className="btn"
+        >
+          {t('Back to message list')}
+        </button>
+      </Link>
+      <button
+        className="btn btn-error"
+        onClick={() => removeMsg(id)}
+      >
+        {t(label)}
+      </button>
+    </div>
+  )
+}
+
+export default Controls
