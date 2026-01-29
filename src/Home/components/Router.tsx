0f795f413b253714974373d5114ab7c2105a9da8
alx <world.zay@gmail.com>
2026-01-29 14:59:43 +0300

    #16 Update message routes

--- src/Home/components/Router.tsx
+++ src/Home/components/Router.tsx
@@ -17,7 +17,9 @@ const Router = () => {
       <Route path='authors' element={<MyAuthors />} />
       <Route path='message'>
         <Route path='list' element={<List />} />
-        <Route path=':id' element={<MessageShow />} />
+        <Route path='in/:id' element={<MessageShow cond='in'/>} />
+        <Route path='out/:id' element={<MessageShow cond='out'/>} />
+        <Route path='del/:id' element={<MessageShow cond='del'/>} />
         <Route path='form' element={<MessageFormWrapper />} />
       </Route>
       <Route path='drafts' element={<Drafts />} />
