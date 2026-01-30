ca09f30f108b28cb52ab81eb0cc917a0fe085caf
alx <world.zay@gmail.com>
2026-01-30 11:20:30 +0300

    #19 Create useAuthorSearch custom hook and apply debouncedSearch

---
+++ src/reused/Participants/hooks/AuthorSearch.ts
@@ -0,0 +1,57 @@
+import { useRef, useState } from "react";
+import { authorsFilterSearch } from "../schema";
+import { debounce } from "../../../common/utils/decorator";
+import { authorsPayloadReset, filterSet, searchSet } from "../store/athorsPayload";
+
+export const useAuthorSearch = () => {
+    const [error, setError] = useState<string | null>()
+    const searchRef = useRef<HTMLInputElement>(null)
+    const filterRef = useRef<HTMLSelectElement>(null)
+
+    const handleSearch = (query: string | null) => {
+        searchSet(query)
+    };
+
+    const debouncedSearch = debounce(handleSearch, 500);
+
+    const onSelect = () => {
+        filterSet(filterRef.current?.value ?? null)
+    }
+
+    const onSearch = () => {
+        const valid = authorsFilterSearch.safeParse(searchRef.current?.value)
+        if (valid.success && valid.data) {
+            setError(null)
+            debouncedSearch(valid.data ?? null)
+        } else if (valid.error) {
+            setError(valid.error.issues[0].message)
+        }
+    }
+
+    const onReset = () => {
+        if (searchRef.current) {
+            searchRef.current.value = ''
+        }
+
+        if (filterRef.current) {
+            filterRef.current.value = ''
+        }
+
+        authorsPayloadReset()
+    }
+
+    const inputClassName =
+        !error ?
+            "input join-item w-full" :
+            "input join-item input-error"
+
+    return {
+        error,
+        onSelect,
+        onSearch,
+        onReset,
+        filterRef,
+        searchRef,
+        inputClassName
+    }
+}
