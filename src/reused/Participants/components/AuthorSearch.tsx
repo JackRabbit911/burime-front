9a55517b2816a8651fb58abdff1bbf3a79077720
alx <world.zay@gmail.com>
2026-01-30 11:22:23 +0300

    #19 Apply useAuthorSearch custom hook

--- src/reused/Participants/components/AuthorSearch.tsx
+++ src/reused/Participants/components/AuthorSearch.tsx
@@ -1,9 +1,6 @@
-import { authorsFilterSearch } from "../schema";
-import { authorsPayloadReset, filterSet, searchSet } from "../store/athorsPayload";
-import { useRef, useState } from "react";
 import { t } from "../../../common/i18n/utils";
 import type { AuthorsPayload } from "../types";
-import Search from "../../icons/Search";
+import { useAuthorSearch } from "../hooks/AuthorSearch";
 
 type Props = {
   filters: string[];
@@ -11,43 +8,15 @@ type Props = {
 }
 
 const AuthorSearch = ({ filters, authorsPayload }: Props) => {
-  const [error, setError] = useState<string | null>()
-  const searchRef = useRef<HTMLInputElement>(null)
-  const filterRef = useRef<HTMLSelectElement>(null)
-
-  const onSelect = () => {
-    filterSet(filterRef.current?.value ?? null)
-  }
-
-  const onSearch = () => {
-    const valid = authorsFilterSearch.safeParse(searchRef.current?.value)
-    if (!valid.success) {
-      setError(valid.error.issues[0].message)
-    } else {
-      setError(null)
-    }
-  }
-
-  const onSearchBtn = () => {
-    searchSet(searchRef.current?.value ?? null)
-  }
-
-  const onReset = () => {
-    if (searchRef.current) {
-      searchRef.current.value = ''
-    }
-
-    if (filterRef.current) {
-      filterRef.current.value = ''
-    }
-
-    authorsPayloadReset()
-  }
-
-  const inputClassName =
-    !error ?
-      "input join-item" :
-      "input join-item input-error"
+  const {
+    error,
+    onSelect,
+    onSearch,
+    onReset,
+    filterRef,
+    searchRef,
+    inputClassName
+  } = useAuthorSearch()
 
   const alert = !error ? null : <div
     className="fieldset-label text-error"
@@ -59,44 +28,32 @@ const AuthorSearch = ({ filters, authorsPayload }: Props) => {
         <div>Search & filter</div>
         {alert}
       </legend>
-      <div className="flex gap-2">
-        <div className="join basis-2/3">
-          <input
-            className={inputClassName}
-            placeholder={t('Search')}
-            defaultValue={authorsPayload.search ?? ''}
-            onChange={onSearch}
-            ref={searchRef}
-          />
-          <button
-            type="button"
-            className="btn btm-sm btn-square join-item"
-            disabled={Boolean(error)}
-            onClick={onSearchBtn}
-          >
-            <Search />
-          </button>
-        </div>
-        <div className="join basis-1/3">
-          <select
-            className="select join-item"
-            defaultValue={authorsPayload.filter ?? ''}
-            onChange={onSelect}
-            ref={filterRef}
-          >
-            <option value="" disabled>Filter</option>
-            {filters?.map((item, key) => (
-              <option key={key} value={item}>{item}</option>
-            ))}
-          </select>
-          <button
-            type="reset"
-            className="btn btm-sm btn-square join-item"
-            onClick={onReset}
-          >
-            X
-          </button>
-        </div>
+      <div className="join">
+        <input
+          className={inputClassName}
+          placeholder={t('Search')}
+          defaultValue={authorsPayload.search ?? ''}
+          onChange={onSearch}
+          ref={searchRef}
+        />
+        <select
+          className="select join-item"
+          defaultValue={authorsPayload.filter ?? ''}
+          onChange={onSelect}
+          ref={filterRef}
+        >
+          <option value="" disabled>Filter</option>
+          {filters?.map((item, key) => (
+            <option key={key} value={item}>{item}</option>
+          ))}
+        </select>
+        <button
+          type="reset"
+          className="btn btm-sm btn-square join-item"
+          onClick={onReset}
+        >
+          X
+        </button>
       </div>
     </ fieldset>
   )
