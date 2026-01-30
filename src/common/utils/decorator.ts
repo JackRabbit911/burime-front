7ba1f4e1c50efa6551fc9033be466b19ca94b344
alx <world.zay@gmail.com>
2026-01-30 12:17:28 +0300

    #19 Create throttle function

--- src/common/utils/decorator.ts
+++ src/common/utils/decorator.ts
@@ -14,3 +14,30 @@ export function debounce<T extends (...args: any[]) => void>(
     }, delay);
   };
 }
+
+type Func = (...args: any[]) => void;
+
+export function throttle<T extends Func>(func: T, wait: number): (...args: Parameters<T>) => void {
+    let isThrottled = false;
+    let savedArgs: Parameters<T> | null = null;
+    let savedThis: any = null;
+
+    return function wrapper(this: any, ...args: Parameters<T>) {
+        if (isThrottled) {
+            savedArgs = args;
+            savedThis = this;
+            return;
+        }
+
+        func.apply(this, args);
+        isThrottled = true;
+
+        setTimeout(() => {
+            isThrottled = false;
+            if (savedArgs) {
+                wrapper.apply(savedThis, savedArgs);
+                savedArgs = savedThis = null;
+            }
+        }, wait);
+    };
+}
