d2d71a77327ede269aada8f793014b1028158d76
alx <world.zay@gmail.com>
2026-02-01 18:14:40 +0300

    #25 Add changeMaster feature

--- src/Branch/components/Authors/index.tsx
+++ src/Branch/components/Authors/index.tsx
@@ -1,14 +1,28 @@
+import { useEffect } from "react";
+import { useFormContext } from "react-hook-form";
+
+import { changeMaster } from "../Form/utils";
+import { getBranchReferenceUri } from "common/constants";
+import Members from "reused/Participants/components/Members";
 import MembersPermissions from "./components/MembersPermissions";
-import type { Bootstrap } from "../../schema/input";
-import AuthorsWrapper from "../../../reused/Participants/components/AuthorsWrapper";
-import Members from "../../../reused/Participants/components/Members";
-import { getBranchReferenceUri } from "../../../common/constants";
+import AuthorsWrapper from "reused/Participants/components/AuthorsWrapper";
+
+import type { Bootstrap } from "Branch/schema/input";
 
 type Props = {
   bootstrap: Bootstrap;
 }
 
 const Authors = ({ bootstrap: { ownAuthors } }: Props) => {
+  const { watch, setValue } = useFormContext()
+  const members = watch('members') || []
+  const masterId = watch('masterId')
+
+  useEffect(() => {
+    const newMembers = changeMaster(members, ownAuthors, masterId)
+    setValue('members', newMembers)
+  }, [masterId])
+
   return (
     <AuthorsWrapper
       ownAuthors={ownAuthors}
