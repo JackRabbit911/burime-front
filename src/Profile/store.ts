a858eb9d20d111cdf2bf4b47427612c25341eae6
alx <world.zay@gmail.com>
2026-02-01 10:49:03 +0300

    #21 Add change password feature and throttles

--- src/Profile/store.ts
+++ src/Profile/store.ts
@@ -1,15 +1,17 @@
 import { createEffect, createEvent, sample } from "effector";
 import ajax from "../common/ajax";
-import { getUserDataUri, saveUserDataUri } from "../common/constants";
-import type { UserData } from "./schema";
+import { getUserDataUri, savePasswordUri, saveUserDataUri } from "../common/constants";
+import type { ConfirmPassword, UserData } from "./schema";
 import type { ApiResponse } from "../common/ajax/types";
 import { pending, throttle } from "patronum";
 import { successDialog } from "../reused/InModal/SuccessDialog";
 import { modalOpened } from "../reused/Modal/store";
 
 export const profileSubmitted = createEvent<UserData>()
+export const passwordSubmitted = createEvent<ConfirmPassword>()
 
-const throttled = throttle(profileSubmitted, 500)
+const throttledProfileSubmitted = throttle(profileSubmitted, 500)
+const throttledPasswordSubmitted = throttle(passwordSubmitted, 500)
 
 export const getUserDataFx = createEffect(async () => {
     const response = await ajax.get<ApiResponse<UserData>>(getUserDataUri)
@@ -21,9 +23,18 @@ const sendProfileFx = createEffect((data: UserData) => (
     ajax.postForm(saveUserDataUri, data)
 ))
 
+const sendPasswordFx = createEffect((data: ConfirmPassword) => (
+    ajax.postForm(savePasswordUri, data)
+))
+
 sample({
-    clock: throttled,
+    clock: throttledProfileSubmitted,
     target: sendProfileFx,
+})
+
+sample({
+    clock: throttledPasswordSubmitted,
+    target: sendPasswordFx,
 })
 
 sample({
@@ -32,6 +43,16 @@ sample({
     fn: () => successDialog({
         text: 'Your profile data was saved',
         link: '',
+    }),
+    target: modalOpened,
+})
+
+sample({
+    source: sendPasswordFx.doneData,
+    filter: (response) => Boolean(response?.data?.success),
+    fn: () => successDialog({
+        text: 'Your password has been changed',
+        link: 'profile',
     }),
     target: modalOpened,
 })
