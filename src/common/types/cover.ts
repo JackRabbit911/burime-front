d1f537597ad0f8e7be2751c421ed3d1f60f764b0
alx <world.zay@gmail.com>
2026-02-03 12:16:49 +0300

    #27 Create cover.ts types file

---
+++ src/common/types/cover.ts
@@ -0,0 +1,16 @@
+export type CoverType = {
+    bg_img: string | null;
+    cover: string | null;
+    bg_color: string;
+    text_color: string;
+    text_size: number;
+}
+
+export type MyBook = {
+    id: number;
+    title: string;
+    cover: CoverType | string;
+    alias: string;
+    genreStr: string;
+    myRole?: number;
+}
