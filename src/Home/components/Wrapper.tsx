f536fbeb42ff685966f089b9668c3aa28c94ce20
alx <world.zay@gmail.com>
2026-02-16 16:33:54 +0300

    #42 Update logout handle

--- src/Home/components/Wrapper.tsx
+++ src/Home/components/Wrapper.tsx
@@ -4,6 +4,7 @@ import { getTitle } from "../utils";
 import { t } from "common/i18n/utils";
 import { useTranslate } from "common/i18n/hook";
 import BreadCrumbs from "./BreadCrumbs";
+import ajax from "common/ajax";
 
 type Props = {
   children?: React.ReactNode;
@@ -15,15 +16,32 @@ const Wrapper = ({ children }: Props) => {
   const addr = uriSegments[0] ?? 'home'
   const id = uriSegments[1]
 
+  const onLogOut = () => {
+    ajax.get('/auth/logout')
+      .then((response) => response.data)
+      .then((data) => {
+        if (data.success) {
+          window.location.href = "/"
+        } else {
+          console.error(data.error)
+        }
+      })
+  }
+
   useTranslate([location])
 
   return (
     <div className="flex flex-row justify-center">
       <div className="w-full md:w-2xl lg:w-4xl bg-base-100 p-4">
         <div className="flex flex-roe justify-between mb-4">
-          <h1 className="text-2xl">{t(getTitle(addr, id))}</h1>
+          <h1 className="text-2xl">{t(getTitle(addr, id))}!</h1>
           {addr === 'home' ?
-            <a href="/auth/logout" className="btn btn-outline btn-error">{t('Log Out')}</a> :
+            <button
+              className="btn btn-outline btn-error"
+              onClick={onLogOut}
+            >
+              {t('Log Out')}
+            </button> :
             <BreadCrumbs pathname={location.pathname} />
           }
         </div>
