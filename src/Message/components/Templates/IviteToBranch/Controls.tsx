b377cdde24be4c61e74c5682226be21119b06f10
alx <world.zay@gmail.com>
2026-03-02 15:18:47 +0300

    #52 Apply useInviteStatus custom hook

--- src/Message/components/Templates/IviteToBranch/Controls.tsx
+++ src/Message/components/Templates/IviteToBranch/Controls.tsx
@@ -1,43 +1,13 @@
 import { t } from "common/i18n/utils";
-import { useParams } from "react-router";
-import { useEffect, useState } from "react";
-import ajax from "common/ajax";
-import { getBranchAuthorStatusUri, setBranchAuthorStatusUri } from "common/constants";
+import { useInviteStatus } from "./hook";
 
 type Props = {
   branch: string | number;
-  author: string | number;
+  author?: number;
 }
 
 const Controls = ({ branch, author }: Props) => {
-  const [status, setStatus] = useState(110)
-  const { box } = useParams()
-  const disabledBoth = box === 'in' ? false : true
-
-  const disabledRefuse = disabledBoth || status === 80
-  const disabledAccept = disabledBoth || status === 200
-
-  const onClick = (status: number) => () => {
-    ajax.postForm(setBranchAuthorStatusUri, {
-      branch_id: branch,
-      author_id: author,
-      status: status,
-    })
-
-    setStatus(status)
-  }
-
-  useEffect(() => {
-    const uri = [getBranchAuthorStatusUri, branch].join('/')
-    const params = { author: author }
-    ajax.get(uri, { params: params })
-      .then((response) => response.data)
-      .then((data) => data.result.status)
-      .then((status) => {
-        setStatus(status)
-      })
-  }, [])
-
+  const { onClick, disabledAccept, disabledRefuse } = useInviteStatus(branch, author)
 
   return (
     <div className="flex justify-center gap-2 mt-2">
