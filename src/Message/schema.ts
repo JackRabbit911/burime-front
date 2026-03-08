4388500669ec524698c12d2abd33f85f1b995d2e
alx <world.zay@gmail.com>
2026-03-08 15:53:16 +0300

    #52 Delete unused type MessageTpl

--- src/Message/schema.ts
+++ src/Message/schema.ts
@@ -29,8 +29,6 @@ const message = z.object({
     data: data,
 })
 
-const messageTpl = message.omit({ subject: true })
-
 export const messageForm = z.object({
     recipients: z.array(recipient),
     message: message,
@@ -44,4 +42,3 @@ export const messageOut = messageForm.extend({
 export type NewRecipient = z.infer<typeof recipient>
 export type MessageForm = z.infer<typeof messageForm>
 export type MessageOut = z.infer<typeof messageOut>
-export type MessageTpl = z.infer<typeof messageTpl>
