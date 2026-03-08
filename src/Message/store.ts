29904a531e534a504bdc69d0c763a42a8e584148
alx <world.zay@gmail.com>
2026-03-08 15:54:53 +0300

    #52 Add getMessageBlankFx and $messageBlank store

--- src/Message/store.ts
+++ src/Message/store.ts
@@ -1,14 +1,15 @@
-import { combine, createEffect, createEvent, createStore, sample } from "effector";
 import { pending } from "patronum";
+import { combine, createEffect, createEvent, createStore, sample } from "effector";
 
 import ajax from "common/ajax";
+import { emptyMessage } from "./utils";
 import { globalReset } from "common/store";
-import type { MessageOut } from "./schema";
 import { modalOpened } from "reused/Modal/store";
-import type { ApiResponse } from "common/ajax/types";
 import { successDialog } from "reused/InModal/SuccessDialog";
-import { getMessageListUri, getMessageUri, saveMessageUri } from "common/constants";
+import { getMessageBlank, getMessageListUri, getMessageUri, saveMessageUri } from "common/constants";
 
+import type { ApiResponse } from "common/ajax/types";
+import type { MessageForm, MessageOut } from "./schema";
 import type { Message, Inbox, MessageList, Outbox, Delbox } from "./types";
 
 export const msgResetted = createEvent()
@@ -22,6 +23,10 @@ export const getMessageListFx = createEffect(
 
 export const getMessageFx = createEffect(
     (id: string) => ajax.get([getMessageUri, id].join('/'))
+)
+
+export const getMessageBlankFx = createEffect(
+    (search: string) => ajax.get([getMessageBlank, search].join('?'))
 )
 
 const saveMessageFx = createEffect((data: MessageOut) => (
@@ -45,6 +50,10 @@ export const $message = createStore<Message | null>(null)
     .reset(msgResetted, globalReset)
 
 export const $toAlias = createStore<string>('Author')
+
+export const $messageBlank = createStore<MessageForm>(emptyMessage)
+    .on(getMessageBlankFx.doneData, (_, response) => response.data.result)
+    .reset(msgResetted, globalReset)
 
 export const $msgCounts = combine({inbox: $inbox, outbox: $outbox, delbox: $delbox}, (store) => ({
     inboxCount: store.inbox.length,
@@ -74,3 +83,4 @@ sample({
 })
 
 export const $isPending = pending([saveMessageFx])
+export const $isPendingBlank = pending([getMessageBlankFx])
