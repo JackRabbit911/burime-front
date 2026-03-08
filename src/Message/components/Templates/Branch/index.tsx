50f2c7a7417adb0d868b3e53acc559fc2779f42a
alx <world.zay@gmail.com>
2026-03-08 16:04:21 +0300

    #52 Create Branch component

---
+++ src/Message/components/Templates/Branch/index.tsx
@@ -0,0 +1,42 @@
+import { t } from "common/i18n/utils";
+import { useUnit } from "effector-react";
+import CoverWrapper from "Message/components/CoverWrapper";
+import { $toAlias } from "Message/store";
+
+import type { Message } from "Message/types";
+
+type Props = {
+  message: Message;
+}
+
+const Branch = ({ message }: Props) => {
+  const toAlias = useUnit($toAlias);
+  const appeal = message.data.appeal ??
+    (message.incoming && Object.hasOwn(message, 'to_alias')) ?
+      message.to_alias : toAlias
+
+  const signature = message.data.signature ?? t('Best regards') + ', ' + message.from_alias
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-2">
+        <CoverWrapper branchId={message.data.branch} />
+        <div className="col-span-2 flex flex-col justify-between">
+          <div>
+            <p>
+              {appeal},
+            </p>
+            <p>
+              {message.data?.body ?? 'no body'}
+            </p>
+          </div>
+          <div className="fieldset mt-1 text-end">
+            {signature}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Branch
