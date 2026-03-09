fe988b8861918dd3619df59675f4d6ace72e7547
alx <world.zay@gmail.com>
2026-03-09 12:55:53 +0300

    #52 Add setValues to useEffect and some changes

--- src/Message/hooks/messageform.ts
+++ src/Message/hooks/messageform.ts
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react"
 import { useUnit } from "effector-react"
+import { useEffect, useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm, type SubmitHandler } from "react-hook-form"
 
-import { $isPending, msgSubmitted } from "../store"
+import { $isPending, msgSubmitted, toAliasSetted } from "../store"
 import { messageForm, messageOut, type MessageForm } from "../schema"
 import { $ownAuthors, getOwnAuthorsFx } from "../../common/store/ownAuthors"
 
-export const useMessageForm = (defaultMsg: MessageForm) => {
+export const useMessageForm = (message: MessageForm) => {
   const [view, setView] = useState('choice')
   const [ownAuthors, isPending] = useUnit([$ownAuthors, $isPending])
 
   const methods = useForm({
     resolver: zodResolver(messageForm),
     mode: 'all',
-    defaultValues: defaultMsg,
+    defaultValues: message,
   })
 
   const onSubmit: SubmitHandler<MessageForm> = (data) => {
@@ -28,23 +28,26 @@ export const useMessageForm = (defaultMsg: MessageForm) => {
       if (valid?.success && valid?.data) {
         msgSubmitted(valid.data)
       }
-
     }
   }
 
   useEffect(() => {
     getOwnAuthorsFx()
 
-    if (methods.getValues('recipients').length > 0) {
+    if (message.recipients.length > 0) {
       setView('form')
     }
   }, [])
-  
-  
+
   useEffect(() => {
-    const from = methods.getValues('message.from')
-    methods.setValue('message.from', from || ownAuthors[0].id)
-  }, [ownAuthors])
+        if (message.recipients.length > 0) {
+          toAliasSetted(message.recipients[0].alias)
+        }
+    
+        methods.setValue('message', message.message)
+        methods.setValue('recipients', message.recipients)
+        methods.setValue('important', message.important)
+      }, [message])
 
   return { methods, ownAuthors, onSubmit, view, setView }
 }
