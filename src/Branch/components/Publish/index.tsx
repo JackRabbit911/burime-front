0710531ca67731b5c8ddb6394c87c527294d782f
alx <world.zay@gmail.com>
2026-03-11 17:29:34 +0300

    #60 Apply Translate feature for publish page

--- src/Branch/components/Publish/index.tsx
+++ src/Branch/components/Publish/index.tsx
@@ -1,15 +1,18 @@
+import { useTranslate } from "common/i18n/hooks";
 import FirstLastPost from "./components/FirstLastPost";
 import Indicator from "./components/Indicator";
 
 const Publish = () => {
+  const __ = useTranslate()
+
   return (
     <fieldset className="fieldset">
       <div className="grid md:grid-cols-3 gap-4">
         <div>
-          <Indicator />
+          <Indicator __={__} />
         </div>
         <div className="md:col-span-2">
-          <FirstLastPost />
+          <FirstLastPost __={__} />
         </div>
       </div>
     </fieldset>
