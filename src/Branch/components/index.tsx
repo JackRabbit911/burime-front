9a1d74301e4d743fa01e40a876eb3ee3e3b1ec6e
alx <world.zay@gmail.com>
2026-03-11 16:01:26 +0300

    #58 Apply useTranslate hook

--- src/Branch/components/index.tsx
+++ src/Branch/components/index.tsx
@@ -4,15 +4,16 @@ import { useUnit } from "effector-react"
 
 import Form from "./Form"
 import Loading from "reused/Loading"
-import { t } from "common/i18n/utils"
 import ErrorCmp from "reused/ErrorCmp"
 import { $status, globalReset } from "common/store"
 import { $bootstrap, getBootstrapFx } from "Branch/store/bootstrap"
+import { useTranslate } from "common/i18n/hooks"
 
 const Branch = () => {
   const { id, draft } = useParams()
   const bootstrap = useUnit($bootstrap)
   const status = useUnit($status)
+  const __ = useTranslate()
 
   useEffect(() => {
     globalReset()
@@ -27,7 +28,7 @@ const Branch = () => {
 
   return bootstrap ? (
     <Form bootstrap={bootstrap}/>
-  ) : <Loading message={t('Loading')} />
+  ) : <Loading message={__('Loading')} />
 }
 
 export default Branch
