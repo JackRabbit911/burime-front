f01c3373bfa12406d50adbb08674271eb176c6a8
alx <world.zay@gmail.com>
2026-03-22 13:30:01 +0300

    #64 Apply useDebounce and useCallback hooks and update context

--- src/common/i18n/TranslateProvider.tsx
+++ src/common/i18n/TranslateProvider.tsx
@@ -1,28 +1,52 @@
-import { createContext, useRef, useState } from "react";
-import type { Argv, ChildrenProps, TranslateContextType, TranslateType } from "./types";
-import { detectLang, sprintf } from "./utils";
+import { createContext, useCallback, useEffect, useRef, useState } from "react";
+
+import { useDebounce } from "./hooks";
+import { sprintf, updateTranslate } from "./utils";
+import { delay, detectLang, getTranslate } from "./config";
+import type { Argv, TranslateContextType, TranslateType } from "./types";
 
 export const TranslateContext = createContext<TranslateContextType | undefined>(undefined)
 
-const TranslateProvider = ({ children }: ChildrenProps) => {
-  const [translate, setTranslate] = useState<TranslateType>({})
-  const translateKeys = useRef<string[]>([])
-  const lang = detectLang()
+type Props = {
+  deps?: React.DependencyList;
+  children?: React.ReactNode;
+}
 
-  const gettext = (key: string, ...argv: Argv) => {
+const TranslateProvider = ({ deps = [], children }: Props) => {
+  const [translate, setTranslate] = useState<TranslateType>({})
+  const [lang, _] = useState<string>(detectLang())
+  const translateKeys = useRef<string[]>([])
+
+  const gettext = useCallback((key: string, ...argv: Argv) => {
     if (Object.hasOwn(translate, key)) {
       return sprintf(translate[key], ...argv)
     }
-
+    
     if (!translate[key] && !translateKeys.current.includes(key)) {
       translateKeys.current.push(key)
     }
 
     return sprintf(key, ...argv)
-  }
+  }, [translate])
+
+  const debouncedFetch = useDebounce((lang, keys) => {
+    getTranslate(lang, keys)
+    .then((result: TranslateType) => {
+      updateTranslate(translate, result, translateKeys, setTranslate)
+    })
+  }, delay)
+  
+  useEffect(() => {
+    const keys = Object.keys(translate)
+    const diff = translateKeys.current.filter(x => !keys.includes(x));
+
+    if (diff.length > 0) {
+      debouncedFetch(lang, translateKeys.current)
+    }
+  }, deps)
 
   return (
-    <TranslateContext.Provider value={{ gettext, translateKeys, translate, setTranslate, lang }}>
+    <TranslateContext.Provider value={{ gettext }}>
       {children}
     </TranslateContext.Provider>
   )
