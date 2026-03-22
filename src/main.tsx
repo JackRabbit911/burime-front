32d3e10b3e6846f2636d80f057310e7457dc4d6b
alx <world.zay@gmail.com>
2026-03-22 12:51:25 +0300

    #64 Add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
+import { detectLang } from 'common/i18n/config.ts'
 
-createRoot(document.getElementById('root')!).render(<App />)
+const prefix = () => {
+    const lang = detectLang()
+
+    return (lang === 'ru') ? '/my' : `/${lang}/my`
+}
+
+createRoot(document.getElementById('root')!).render(
+    <BrowserRouter basename={prefix()}>
+        <App />
+    </BrowserRouter>
+)
