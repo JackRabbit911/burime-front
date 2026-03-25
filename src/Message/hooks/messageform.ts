5dfa07786f9e170156a73db9dd5f9d185cb779e0
alx <world.zay@gmail.com>
2026-03-25 16:12:22 +0300

    #66 Add getOwnAuthorsFx

--- src/Message/hooks/messageform.ts
+++ src/Message/hooks/messageform.ts
@@ -6,10 +6,13 @@ import { useForm, type SubmitHandler } from "react-hook-form"
 import { messageForm, messageOut, type MessageForm } from "../schema"
 import { $ownAuthors, getOwnAuthorsFx } from "../../common/store/ownAuthors"
 import { $isPending, $viewMsgForm, msgSubmitted, setMsgView, toAliasSetted } from "../store"
+import { $authorsPayload } from "reused/Participants/store/athorsPayload"
+import { getAuthorsFx } from "reused/Participants/store/authors"
 
 export const useMessageForm = (message: MessageForm) => {
   const view = useUnit($viewMsgForm)
   const [ownAuthors, isPending] = useUnit([$ownAuthors, $isPending])
+  const authorsPayload = useUnit($authorsPayload)
 
   const methods = useForm({
     resolver: zodResolver(messageForm),
@@ -32,7 +35,9 @@ export const useMessageForm = (message: MessageForm) => {
   }
 
   useEffect(() => {
-    getOwnAuthorsFx()
+    if (ownAuthors.length === 0) {
+      getOwnAuthorsFx()
+    }
   }, [])
 
   useEffect(() => {
@@ -42,14 +47,18 @@ export const useMessageForm = (message: MessageForm) => {
   }, [message.recipients])
 
   useEffect(() => {
-        if (message.recipients.length > 0) {
-          toAliasSetted(message.recipients[0].alias)
-        }
-    
-        methods.setValue('message', message.message)
-        methods.setValue('recipients', message.recipients)
-        methods.setValue('important', message.important)
-      }, [message])
+    if (message.recipients.length > 0) {
+      toAliasSetted(message.recipients[0].alias)
+    }
+
+    methods.setValue('message', message.message)
+    methods.setValue('recipients', message.recipients)
+    methods.setValue('important', message.important)
+  }, [message])
+
+  useEffect(() => {
+    getAuthorsFx(authorsPayload)
+  }, [authorsPayload])
 
   return { methods, ownAuthors, onSubmit, view }
 }
