30902e2fe72b761c9680e8b9fd845b964fd9b58d
alx <world.zay@gmail.com>
2026-04-07 15:02:04 +0300

    #73 Remove isPending and idLoading conditions

--- src/Profile/components/index.tsx
+++ src/Profile/components/index.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router";
-import { useUnit } from "effector-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormProvider, useForm, type SubmitHandler } from "react-hook-form"
 
@@ -8,62 +7,51 @@ import Header from "./Header";
 import { isObjectEmpty } from "common/utils";
 import { useTranslate } from "common/i18n/hooks";
 import { userData, type UserData } from "../schema";
-import { $isPending, getUserDataFx, profileSubmitted } from "../store";
+import { getUserDataFx, profileSubmitted } from "../store";
 import CSRF from "reused/CSRF";
+import { useUnit } from "effector-react";
+import { $status } from "common/store";
 
 const Profile = () => {
-  const isPending = useUnit($isPending)
+  const status = useUnit($status)
   const methods = useForm({
     resolver: zodResolver(userData),
     mode: 'all',
     defaultValues: () => getUserDataFx()
   });
-  const __ = useTranslate()
 
   const onSubmit: SubmitHandler<UserData> = (data) => {
-    if (!isPending ) {
-      const valid = userData.safeParse(data)
-
-      if (valid?.error) {
-        console.log(valid.error, data)
-      }
-
-      if (valid?.success && valid?.data) {
-        profileSubmitted(valid.data)
-      }
-    }
+    profileSubmitted(data)
   }
+
+  const __ = useTranslate()
 
   const disabled = !isObjectEmpty(methods.formState.errors)
 
-  if (methods.formState.isLoading) {
-    return <p>Loading form data...</p>;
-  }
+  console.log(status)
 
   return (
-    <>
-      <FormProvider {...methods}>
-        <Header />
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <CSRF />
-          <Form __={__} />
-          <button
-            type="submit"
-            className="btn btn-primary dark:btn-info w-full mt-4"
-            disabled={disabled}
-          >
-            {__('Save')}
+    <FormProvider {...methods}>
+      <Header />
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <CSRF />
+        <Form __={__} />
+        <button
+          type="submit"
+          className="btn btn-primary dark:btn-info w-full mt-4"
+          disabled={disabled}
+        >
+          {__('Save')}
+        </button>
+      </form>
+      <div className="text-end">
+        <Link to='/profile/password'>
+          <button className="link mt-4">
+            {__('Change password')}
           </button>
-        </form>
-        <div className="text-end">
-          <Link to='/profile/password'>
-            <button className="link mt-4">
-              {__('Change password')}
-            </button>
-          </Link>
-        </div>
-      </FormProvider>
-    </>
+        </Link>
+      </div>
+    </FormProvider>
   )
 }
 
