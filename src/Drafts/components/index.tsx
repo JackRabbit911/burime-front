432967858fd1209d7c0cdddc4133a4017a8f3d58
alx <world.zay@gmail.com>
2026-04-08 13:20:24 +0300

    #75 Add getText and apply ErrorOrPending wrapper

--- src/Drafts/components/index.tsx
+++ src/Drafts/components/index.tsx
@@ -1,16 +1,22 @@
-import { useList } from "effector-react"
-import { $myDrafts, getMyDraftsFx } from "../store"
 import { useEffect } from "react"
 import { Link } from "react-router"
+import { useList, useUnit } from "effector-react"
+
+import { $status } from "common/store"
+import { useTranslate } from "common/i18n/hooks"
+import ErrorOrPending from "reused/ErrorOrPendig"
+import { $isPending, $myDrafts, getMyDraftsFx } from "../store"
 
 const Drafts = () => {
+  const [status, isLoading] = useUnit([$status, $isPending])
+  const __ = useTranslate()
   const myDrafts = useList($myDrafts, {
     fn: (draft) => (
       <Link to={`/branch/${draft.id}/draft`}>
         <li>{draft.title}</li>
       </Link>
     ),
-    placeholder: <h2>Здесь пока ничего нет</h2>
+    placeholder: __("There's nothing here yet")
   })
 
   useEffect(() => {
@@ -18,9 +24,11 @@ const Drafts = () => {
   }, [])
 
   return (
+    <ErrorOrPending status={status} isLoading={isLoading}>
       <ul className="link">
         {myDrafts}
       </ul>
+    </ErrorOrPending>
   )
 }
 
