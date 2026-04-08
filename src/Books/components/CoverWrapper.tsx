e7f73bbf7b83d60d657da23b8448ab3c60c416d9
alx <world.zay@gmail.com>
2026-04-08 17:25:24 +0300

    #75 Add and apply getText as prop

--- src/Books/components/CoverWrapper.tsx
+++ src/Books/components/CoverWrapper.tsx
@@ -1,12 +1,14 @@
+import type { GetText } from "common/i18n/types";
 import { Link } from "react-router";
 
 type Props = {
+  __: GetText;
   id: number;
   role: number;
   children?: React.ReactNode;
 }
 
-const CoverWrapper = ({ id, role, children }: Props) => {
+const CoverWrapper = ({ __, id, role, children }: Props) => {
   const branchEditor = (1 << 6) | (1 << 7)
 
   return (
@@ -17,7 +19,7 @@ const CoverWrapper = ({ id, role, children }: Props) => {
       {(role & branchEditor) ?
         <Link to={`/branch/${id}`}>
           <button className="btn btn-sm btn-outline w-full">
-            Edit branch
+            {__('Edit branch')}
           </button>
         </Link> : null}
     </div>
