5b3fa3f2a07e78dd56a12725c29511a7bb820abe
alx <world.zay@gmail.com>
2026-04-25 16:52:21 +0300

    #90 Update templates

--- src/Message/components/List/Inbox/index.tsx
+++ src/Message/components/List/Inbox/index.tsx
@@ -3,7 +3,6 @@ import { useList, useUnit } from "effector-react"
 
 import MsgLine from "./MsgLine"
 import TableHead from "../TableHead"
-// import { t } from "common/i18n/utils"
 import { $inbox, $msgCounts } from "Message/store"
 import type { GetTextProp } from "common/i18n/types"
 
@@ -12,30 +11,32 @@ const Inbox = ({ __ }: GetTextProp) => {
   const { inboxCount, outboxCount, delboxCount } = useUnit($msgCounts)
 
   return (
-    <div role="tablist" className="tabs tabs-lift w-full">
-      <a
-        role="tab"
-        className="tab tab-active"
+      <div role="tablist"
+        className="tabs tabs-lift w-full"
       >
-        {__('Inbox')} ({inboxCount})
-      </a>
-      <div role="tabpanel" className="w-full tab-content border-base-300 rounded-btn p-6">
-        <div className="overflow-x-auto">
-          <table className="table table-sm md:table-md">
-            <TableHead __={__} />
-            <tbody>
-              {inbox}
-            </tbody>
-          </table>
+        <a
+          role="tab"
+          className="lg:[--tab-bg:red] tab tab-active"
+        >
+          {__('Inbox')} ({inboxCount})
+        </a>
+        <div role="tabpanel" className="w-full tab-content border-base-300 rounded-btn p-6">
+          <div className="overflow-x-auto">
+            <table className="table table-sm md:table-md">
+              <TableHead __={__} />
+              <tbody>
+                {inbox}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-      <Link to='/message/outbox' role="tab" className="tab">
-        {__('Outbox')} ({outboxCount})
-      </Link>
-      <Link to='/message/deleted' role="tab" className="tab">
-        {__('Deleted')} ({delboxCount})
-      </Link>
-    </div >
+        <Link to='/message/outbox' role="tab" className="tab">
+          {__('Outbox')} ({outboxCount})
+        </Link>
+        <Link to='/message/deleted' role="tab" className="tab">
+          {__('Deleted')} ({delboxCount})
+        </Link>
+      </div >
   )
 }
 
