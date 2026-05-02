d69acffae31d9fa970472e7a968a85c2824043b3
alx <world.zay@gmail.com>
2026-05-02 12:06:12 +0300

    #98 Change usage serverErrorRecieved by import directly

--- src/common/hook.ts
+++ src/common/hook.ts
@@ -1,15 +1,13 @@
 import { useEffect } from "react";
-import type { EventCallable } from "effector";
 import type { FieldValues, Path, UseFormSetError } from "react-hook-form";
 
-import type { ValidationError } from "./ajax/types";
+import { serverErrorRecieved } from "./store";
 
 export const useServerErrors = <T extends FieldValues>(
     setError: UseFormSetError<T>,
-    errorEvent: EventCallable<ValidationError[] |undefined>
 ) => {
     useEffect(() => {
-        const unwatch = errorEvent.watch((errors) => {
+        const unwatch = serverErrorRecieved.watch((errors) => {
             errors?.forEach(({ key, msg }) => {
                 // Используем as Path<T>, чтобы TS разрешил динамический ключ
                 setError(key as Path<T>, {
@@ -20,5 +18,5 @@ export const useServerErrors = <T extends FieldValues>(
         });
 
         return () => unwatch();
-    }, [setError, errorEvent])
+    }, [setError, serverErrorRecieved])
 }
