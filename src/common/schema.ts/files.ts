c47b7ea3e5d7833eb65cf3fc4c4f7ccf4c4facff
alx <world.zay@gmail.com>
2026-05-03 12:33:20 +0300

    #100 Add custom messages to schema

--- src/common/schema.ts/files.ts
+++ src/common/schema.ts/files.ts
@@ -4,6 +4,6 @@ const MAX_UPLOAD_SIZE = 1024 * 1024 * 2; // 2MB
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/png", "image/gif"]
 
 export const imageFile = z.file()
-    .max(MAX_UPLOAD_SIZE)
-    .mime(ACCEPTED_IMAGE_TYPES)
+    .max(MAX_UPLOAD_SIZE, {message: 'The file size exceeds the allowed limit'})
+    .mime(ACCEPTED_IMAGE_TYPES, {message: 'The file must be an image'})
     .nullable()
