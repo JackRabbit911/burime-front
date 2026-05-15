e659c5eed2081a5511d5b92ac93bd90e42c975cc
alx <world.zay@gmail.com>
2026-05-15 18:10:44 +0300

    #104 Apply useAuthorsWrapper custom hook

--- src/reused/Participants/components/AuthorsWrapper.tsx
+++ src/reused/Participants/components/AuthorsWrapper.tsx
@@ -1,15 +1,11 @@
-import { useEffect } from "react"
 import { useUnit } from "effector-react"
-import { useFormContext } from "react-hook-form"
-import { $authorsList, $memberId } from "../store/authors"
-import { $authorsPayload } from "../store/athorsPayload"
-import { $referenceBooks, referenceRecived } from "../store/reference"
-import { addGroupMembers, addNewMember } from "../utils"
-import type { Author } from "../schema"
-import { getGroupMembersFx } from "../store/groupMembers"
-import type { OwnAuthor } from "../types"
+
 import Select from "./Select"
 import AuthorsChoice from "./AuthorsChoice"
+import { $memberId } from "../store/authors"
+import { useAuthorsWrapper } from "../hooks/authorsWrapper"
+
+import type { OwnAuthor } from "../types"
 
 type Props = {
   ownAuthors: OwnAuthor[];
@@ -20,33 +16,7 @@ type Props = {
 
 const AuthorsWrapper = ({ ownAuthors, choiceList, permissions, referenceUri }: Props) => {
   const memberId = useUnit($memberId)
-  const authorsList = useUnit($authorsList)
-  const authorsPayload = useUnit($authorsPayload)
-  const referenceBooks = useUnit($referenceBooks)
-  const authorsFilters = referenceBooks?.authorsFilters || []
-
-  const { watch, setValue } = useFormContext()
-  const members = watch('members') || []
-
-  const onChoiceAuthor = (author: Author) => {
-    const newMembers = addNewMember(members, author)
-    setValue('members', newMembers, { shouldValidate: true, shouldDirty: true })
-  }
-
-  const onChoiceGroup = (author: Author) => {
-    const promise = getGroupMembersFx(author.id)
-    promise.then((response) => response.data.result)
-      .then((result) => {
-        const newMembers = addGroupMembers(members, result)
-        setValue('members', newMembers, { shouldValidate: true, shouldDirty: true })
-      })
-  }
-
-  const onChoice = authorsPayload.filter === 'groups' ? onChoiceGroup : onChoiceAuthor
-
-  useEffect(() => {
-    referenceRecived(referenceUri)
-  }, [])
+  const [members, authorsPayload, onChoice] = useAuthorsWrapper(referenceUri)
 
   return (
     <div className="grid md:grid-cols-3 gap-4">
@@ -59,19 +29,17 @@ const AuthorsWrapper = ({ ownAuthors, choiceList, permissions, referenceUri }: P
                 label="Team leader"
                 options={ownAuthors}
               /> : null}
-              {choiceList}
+            {choiceList}
           </div>
           <div className="md:col-span-2">
             <AuthorsChoice
-              filters={authorsFilters}
-              authors={authorsList}
               members={members}
               authorsPayload={authorsPayload}
               handler={onChoice}
             />
           </div>
         </> : permissions
-        }
+      }
     </div>
   )
 }
