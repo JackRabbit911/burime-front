bb696cab32b398383ec8ffee9d94dcd4505e22f0
alx <world.zay@gmail.com>
2026-05-22 14:21:41 +0300

    #108 Use ownAuthors instead bootstrap and update render condition

--- src/Branch/components/Form/index.tsx
+++ src/Branch/components/Form/index.tsx
@@ -14,21 +14,23 @@ import { $step } from "Branch/store/step";
 import StepControls from "../StepControls";
 import { useTranslate } from "common/i18n/hooks";
 import { formSchema } from "Branch/schema/output";
+import { $ownAuthors } from "common/store/ownAuthors";
 
-import type { Bootstrap } from "Branch/schema/input";
+import type { BootstrapStore } from "Branch/schema/input";
 
 type Props = {
-  bootstrap: Bootstrap;
+  bootstrap: BootstrapStore;
 }
 
 const Form = ({ bootstrap }: Props) => {
   const step = useUnit($step)
+  const ownAuthors = useUnit($ownAuthors)
   const branchGenres = bootstrap?.branch_genres || [];
 
   const methods = useForm({
     resolver: zodResolver(formSchema),
     mode: "all",
-    defaultValues: getDefaults(bootstrap)
+    defaultValues: getDefaults(bootstrap, ownAuthors)
   });
 
   const __ = useTranslate()
@@ -37,11 +39,11 @@ const Form = ({ bootstrap }: Props) => {
     <FormProvider {...methods}>
       <Title __={__} />
       <Steps __={__} />
-      <Genres step={step} genres={bootstrap?.total_genres || []} checked={branchGenres} />
-      <Rules step={step} __={__} />
-      {step === 3 ? <Authors bootstrap={bootstrap} /> : null}
-      {step === 4 ? <Cover /> : null}
-      {step === 5 ? <Publish /> : null}
+      {step === 1 && <Genres genres={bootstrap?.total_genres || []} checked={branchGenres} />}
+      {step === 2 && <Rules __={__} />}
+      {step === 3 && <Authors ownAuthors={ownAuthors} />}
+      {step === 4 && <Cover />}
+      {step === 5 && <Publish />}
       <StepControls step={step} __={__} />
     </FormProvider>
   )
