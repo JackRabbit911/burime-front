9e51ecb13d863faa96f30f04cf9761a717474c78
alx <world.zay@gmail.com>
2026-05-22 12:58:50 +0300

    #108 Delete referenseUri property

--- src/Branch/components/Authors/index.tsx
+++ src/Branch/components/Authors/index.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 
 import { changeMaster } from "../Form/utils";
-import { getBranchReferenceUri } from "common/constants";
 import Members from "reused/Participants/components/Members";
 import MembersPermissions from "./components/MembersPermissions";
 import AuthorsWrapper from "reused/Participants/components/AuthorsWrapper";
@@ -28,7 +27,6 @@ const Authors = ({ bootstrap: { ownAuthors } }: Props) => {
       ownAuthors={ownAuthors}
       choiceList={<Members />}
       permissions={<MembersPermissions/>}
-      referenceUri={getBranchReferenceUri}
   />)
 }
 
