d150c5d91ab360a7905b4b26fd3450789189b01a
alx <world.zay@gmail.com>
2026-06-09 17:43:32 +0300

    #112 Add some defaultTranslateKeys

--- src/common/i18n/config.ts
+++ src/common/i18n/config.ts
@@ -21,7 +21,7 @@ const DETECT_LANG_METHOD = 'subdomain'
 export const defaultTranslateKeys = [
     'Title is required', 'Loading', 'Edit author', 'Edit branch',
     'The file size exceeds the allowed limit',
-    'The file must be an image',
+    'The file must be an image', 'friends', 'favorites', 'addressbook', 'groups',
 ]
 
 export const limit = null //cache limit in pairs key-valaue
