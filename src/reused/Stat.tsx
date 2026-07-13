7605c6daa4d559780fe2a1c335fbc6cb8a9c6655
alx <world.zay@gmail.com>
2026-07-13 11:00:27 +0300

    #118 Remove classMap const, update props type

--- src/reused/Stat.tsx
+++ src/reused/Stat.tsx
@@ -1,39 +1,17 @@
 import { Link } from "react-router"
 import type { ReactNode } from "react";
+import { classMap } from "common/constants";
 
 type Props = {
   path: string,
-  title: string,
-  value: string | number,
+  title?: string,
+  value?: string | number,
   color?: string,
   icon?: ReactNode,
   desc?: string,
 };
 
-const Stat = ({ path, title, value, icon, color = 'default', desc = '' }: Props) => {
-  const classMap: { [key: string]: { [key: string]: string } } = {
-    figure: {
-      primary: "stat-figure text-primary",
-      secondary: "stat-figure text-secondary",
-      info: "stat-figure text-info",
-      accent: "stat-figure text-accent",
-      success: "stat-figure text-success",
-      warning: "stat-figure text-warning",
-      error: "stat-figure text-error",
-      default: "stat-figure",
-    },
-    value: {
-      primary: "text-start stat-value text-primary",
-      secondary: "text-start stat-value text-secondary",
-      info: "text-start stat-value text-info",
-      accent: "text-start stat-value text-accent",
-      success: "text-start stat-value text-success",
-      warning: "text-start stat-value text-warning",
-      error: "text-start stat-value text-error",
-      default: "text-start stat-value",
-    },
-  }
-
+const Stat = ({ path, title = '', value = '', icon = '', color = 'default', desc = '' }: Props) => {
   return (
     <div className="text-center border border-zinc-600 rounded-sm">
       <div className="stats">
