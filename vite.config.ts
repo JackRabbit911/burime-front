bff0228e93fdb4cbdecb21c14a4ff36777993888
alx <world.zay@gmail.com>
2026-07-13 11:21:38 +0300

    #120 Add base and server sections

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,22 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), tsconfigPaths(),react()],
+  base: '/my/',
+  server: {
+    host: true, // Позволяет Docker пробрасывать порт наружу
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      // Все запросы, начинающиеся с /api, пойдут на бэкенд
+      '/api': {
+        target: 'http://ru.localhost:80',
+        changeOrigin: true,
+        secure: false,
+      }
+    },
+    hmr: {
+      host: 'ru.localhost',
+      protocol: 'ws',
+    }
+  }
 })
